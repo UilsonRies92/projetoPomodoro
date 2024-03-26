29a09713ed929f8baab9b71d96130cc402d32c52
Uilson Ries <uilson_ries@estudante.sesisenai.org.br>
2024-03-26 11:19:12 -0300

    Cronômetro inicial

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+var mm = 0;
+var ss = 0;
+
+var tempo = 10; //conversão milésimos para segundo
+var cron;
+
+function start(){
+cron = setInterval(() => {timer();}, tempo);
+
+}
+
+function pause(){
+    
+    clearInterval(cron);
+
+}
+function stop(){
+    clearInterval(cron);
+    mm = 0;
+    ss = 0;
+
+    document.getElementById('contador').innerText = "00:00"
+}
+
+function timer(){
+    ss++;
+
+    if (ss == 60){
+        ss = 0;
+        mm++;
+    }
+    var format =(mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss); //botar a esquerda
+    document.getElementById('contador').innerText = format;
+    if (mm >= 25){
+        clearInterval(cron);
+        mm = 0;
+        ss = 0;
+        document.getElementById('contador').innerText = "00:00"
+}}
